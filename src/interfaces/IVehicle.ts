edfd9b08d30de080dedc622390f4964081e6c496
GustavoGracioM <gustavogracio@hotmail.com>
2022-09-14 16:53:05 -0300

    Cria model Car

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const vehicleZodSchema = z.object({
+export const VehicleZodSchema = z.object({
   model: z.string().min(3),
   year: z.number().gte(1900).lte(2022),
   color: z.string().min(3),
@@ -8,4 +8,4 @@ const vehicleZodSchema = z.object({
   buyValue: z.number().int(),
 });
 
-export type IVehicle = z.infer<typeof vehicleZodSchema>;
+export type IVehicle = z.infer<typeof VehicleZodSchema>;
