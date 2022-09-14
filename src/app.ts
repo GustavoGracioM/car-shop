4844a5eab26526521f9e5c9ea81d4039373c9c20
GustavoGracioM <gustavogracio@hotmail.com>
2022-09-14 17:18:50 -0300

    Cria rotas de Car

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import carRouter from './router/Car';
 
 const app = express();
+app.use(express.json());
+app.use('/car', carRouter);
 
 export default app;
---
+++ src/router/Car.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/Car';
+import CarModel from '../models/Car';
+import CarService from '../services/Car';
+
+const router = Router();
+
+const carModel = new CarModel();
+const carService = new CarService(carModel);
+const carContoller = new CarController(carService);
+
+router.post('/', (req, res) => carContoller.create(req, res));
+
+export default router;
