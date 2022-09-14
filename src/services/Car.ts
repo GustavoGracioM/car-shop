0abdc6fc543ba6262e1be2c7b23df1e48b34e93b
GustavoGracioM <gustavogracio@hotmail.com>
2022-09-14 16:55:50 -0300

    Cria interface IService

--- src/services/Car.ts
+++ src/services/Car.ts
@@ -1,7 +1,8 @@
 import { CarZodSchema, ICar } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
+import { IService } from '../interfaces/IService';
 
-export default class CarService {
+export default class CarService implements IService<ICar> {
   constructor(private _car: IModel<ICar>) {}
 
   public async create(obj: unknown): Promise<ICar> {
