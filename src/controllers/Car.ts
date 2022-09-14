6b6464eeee845a748163e90e6b9f421c036d9a1c
GustavoGracioM <gustavogracio@hotmail.com>
2022-09-14 17:01:55 -0300

    Cria controller Car

---
+++ src/controllers/Car.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { ICar } from '../interfaces/ICar';
+import { IService } from '../interfaces/IService';
+
+export default class CarController {
+  constructor(private _service: IService<ICar>) {}
+
+  public async create(req: Request, res: Response<ICar>) {
+    const { model, year, color, status, buyValue, doorsQty, seatsQty } = req.body;
+    const car = { model, year, color, status, buyValue, doorsQty, seatsQty };
+    const created = await this._service.create(car);
+    res.status(200).json(created);
+  }
+}
