d194371853be906ff56048e1ba49c80f1245ce96
GustavoGracioM <gustavogracio@hotmail.com>
2022-09-15 14:46:58 -0300

    Cria interface IMotocycle

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const typeMotorcycle = ['Street', 'Custom', 'Trail'] as const;
+
+export const MotorcycleZodSchema = VehicleZodSchema.extend({
+  category: z.enum(typeMotorcycle),
+  engineCapacity: z.number().int().positive().lte(2500)
+    .lte(7),
+});
+
+export type IMotorcycle = z.infer<typeof MotorcycleZodSchema >;
